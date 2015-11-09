b1e26de7eb155405267191675efe26772ce9ea2d
sergiy-turchyn <sergey.turchyn@gmail.com>
2015-11-08 22:57:01 -0500

    Added node creation

--- interface/d3code.js
+++ interface/d3code.js
@@ -7,6 +7,9 @@ var circleRadius = 20;
 var nodes = [];
 var edges = [];
 var conn_edges = [];
+var maxNodeID = -1;
+var maxEdgeID = -1;
+var maxConnEdgeID = -1;
 var nodeTemplate = {
 	id: -1,
 	text: "",
@@ -22,27 +25,18 @@ var edgeTemplate = {
 	type: 1
 }
 
-//This is for testing, to be removed when we can create new nodes ourselves
-nodes.push({
-	id: 0,
-	text: "test",
-	x:0.5,
-	y:0.5,
-	type:0
-});
-nodes.push({
-	id: 1,
-	text: "test2",
-	x:0.2,
-	y:0.3,
-	type:0
-});
+//Set default mode
+var mode;
+var defaultModeButton = document.getElementById("defaultModeButton");
+defaultModeButton.checked = true;
+mode = defaultModeButton.value;
 	
 //Create svg
 var svg = d3.select(".content")
             .append("svg")
             .attr("width", w)   
-            .attr("height", h);
+            .attr("height", h)
+            .on("click", onclick);
 var circles;
 var labels;
 var edges;
@@ -51,13 +45,17 @@ var edge_labels;
 
 updateSVG();
 
+//Get the text field
+var labelTextField = document.getElementById("label");
+
 //Create SVG given data
-function updateSVG() {            
+function updateSVG() {   
 	//Create node circles
 	circles = svg.selectAll("circle")
 		.data(nodes)
 		.enter()
 		.append("circle")
+		.on("click", onclick)
 		.attr("class", "circle")
 		.attr("transform", function(d) {
 			return "translate(" + (d.x*w) + "," + (d.y*h) + ")";
@@ -69,6 +67,7 @@ function updateSVG() {
 		.data(nodes)
 		.enter()
 		.append("text")
+		.on("click", onclick)
 		.attr("class", "label")
 		.text(function(d) {
 			return d.text;
@@ -82,7 +81,45 @@ function updateSVG() {
 	//Create edge label nodes
 }
 
-//Handle mode change
-function changeMode(mode) {
-    console.log('New mode: ' + mode.value);
+//Handle mouse click
+//Note: if we click on a circle, then the function is called twice (from the circle & background)
+//Not sure how to check if there is an object at the coordinates
+function onclick(d) {
+	console.log(mode);
+	var coords = d3.mouse(this);
+	var x = coords[0];
+	var y = coords[1];
+	if (d != undefined) {
+		//Clicked on an existing object
+		console.log("this is an object");
+	}
+	else {
+		switch(mode) {
+			case "node":
+				maxNodeID += 1;
+				nodes.push({
+					id: maxNodeID,
+					text: "test" + maxNodeID,
+					x:x/w,
+					y:y/h,
+					type:0
+				});
+				updateSVG();
+				break;
+			case "edge":
+				console.log("should create a new edge");
+				break;
+			case "conn_edge":
+				console.log("should create a new connection edge");
+				break;
+			case "select":
+			default:
+				break;
+		}
+	}	
+}
+
+//Change mode
+function changeMode(r) {
+	mode = r.value;
 }
